95748bf78ca19562715ea496d0716f02b7a9dbbc
Turux <35221455+Turux@users.noreply.github.com>
2018-01-10 17:42:17 +0000

    Reduced filtering window
    Filtering window now set to half of a second. 
    Original window 2 seconds

--- buildtimetable.m
+++ buildtimetable.m
@@ -3,6 +3,8 @@ function [ timetable ] = buildtimetable( rawtable,fs,arm )
 %   Detailed explanation goes here
 
 timetable = table(rawtable.angle, 'VariableNames', {'AngleDeg'});
+%timetable.AngleDeg = timetable.AngleDeg + 90;
+%timetable.AngleDeg = wrapTo360(timetable.AngleDeg);
 timetable.AngleRad = deg2rad(timetable.AngleDeg);
 [AngSec, AngG] = deg2G(timetable.AngleDeg);
 timetable.AngleSector = AngSec;
@@ -10,15 +12,15 @@ timetable.AngleG = AngG;
 timetable.Revolution = deg2revs(timetable.AngleDeg);
 timetable.CadenceRads = rad2rads(timetable.AngleRad,fs);
 timetable.CadenceRPM = deg2rpm(timetable.AngleDeg,fs);
-timetable.CadenceFiltRads = movmean(timetable.CadenceRads, 2*fs);
-timetable.CadenceFiltRPM = movmean(timetable.CadenceRPM, 2*fs);
+timetable.CadenceFiltRads = movmean(timetable.CadenceRads, fs/2);
+timetable.CadenceFiltRPM = movmean(timetable.CadenceRPM, fs/2);
 timetable.TanForceLeftN = rawtable.Fe_l;
 timetable.TanForceRightN = rawtable.Fe_r;
 timetable.RadForceLeftN = rawtable.Fu_l;
 timetable.RadForceRightN = rawtable.Fu_r;
 timetable.NetTanForceN = timetable.TanForceLeftN + timetable.TanForceRightN;
 timetable.TorqueNm = timetable.NetTanForceN.*arm;
-timetable.TorqueFiltNm = movmean(timetable.TorqueNm,2*fs);
+timetable.TorqueFiltNm = movmean(timetable.TorqueNm,fs/2);
 timetable.PowerW = timetable.TorqueNm.*timetable.CadenceFiltRads;
 timetable.PowerFiltW = timetable.TorqueFiltNm.*timetable.CadenceFiltRads;
 timetable.BalanceLR = (abs(timetable.TanForceLeftN)./(abs(timetable.TanForceLeftN)+abs(timetable.TanForceRightN)))*100;
